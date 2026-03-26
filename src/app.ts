bce962a0523ed20ddbeef431cf2b01493e2f18f2
Chandilene <chandileneborges@gmail.com>
2026-03-26 14:36:18 -0300

    feat: criação da classe de error, instalação do zod, express-async-errors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express from "express";
+import { errorHandling } from "@/middlewares/error-handling";
+import "express-async-errors";
 
 const app = express();
 app.use(express.json());
+app.use(errorHandling);
 
 export { app };
