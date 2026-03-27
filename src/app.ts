c8f73411d33d2e60bda54c2ecdd0ac8e77cbcc18
Chandilene <chandileneborges@gmail.com>
2026-03-27 11:20:10 -0300

    feat: Validando dados do usuario

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import express from "express";
-import { errorHandling } from "@/middlewares/error-handling";
-import { routes } from "./routes";
 import "express-async-errors";
+import { routes } from "./routes";
+import { errorHandling } from "@/middlewares/error-handling";
 
 const app = express();
 app.use(express.json());
-app.use(errorHandling);
 app.use(routes);
+app.use(errorHandling);
 
 export { app };
